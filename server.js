30b294da2bd07b7226d84fe28511f01edcc4356f
pankajmokashi <pankajmokashi12@gmail.com>
2024-08-04 21:01:33 +0530

    port update

--- server.js
+++ server.js
@@ -14,5 +14,5 @@ app.use(express.json());
 app.use("/api/groups", groupRoutes);
 app.use("/api/notes", noteRoutes);
 
-const port = process.env.PORT;
-app.listen(port, () => console.log(`Server running on port ${PORT}`));
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server running on port ${port}`));
